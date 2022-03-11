01f87b9643bffa557c8bca6f81f7d3817bc7091c
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-11 12:38:27 +0600

    3 bugs fixed already!

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ const reportPost = (id) => {
 
 const displayContent = (text) => {
   return text.length < 30
-    ? "text"
+    ? text
     : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
@@ -56,6 +56,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  console.log(post.userImage)
   const image = post.image;
   const div = document.createElement("article");
   div.classList.add("post");
@@ -67,7 +68,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -129,9 +130,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0].user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
